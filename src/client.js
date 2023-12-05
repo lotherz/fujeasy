afd26cc97753769a7fa9996d30265a5943dc5644
Kyle Lotherington <kyle.lotherington@gmail.com>
2023-12-05 19:22:46 +1100

    fixed click location

--- src/client.js
+++ src/client.js
@@ -5,7 +5,7 @@ client.connect(8080, '192.168.1.14', () => {
   console.log('Connected to VM');
 
   // Example: Move mouse
-  client.write(JSON.stringify({ type: 'click', x: 796, y: 100 }));
+  client.write(JSON.stringify({ type: 'click', x: 796, y: 618 }));
 });
 
 client.on('data', (data) => {
