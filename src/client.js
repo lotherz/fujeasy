fcc5db9d05e67df9a08d40825383dc293808f241
Kyle Lotherington <kyle.lotherington@gmail.com>
2023-12-06 12:09:00 +1100

    removed screenshot option

--- src/client.js
+++ src/client.js
@@ -4,7 +4,6 @@ const client = new net.Socket();
 client.connect(8080, '192.168.1.14', () => {
   console.log('Connected to VM');
 
-  // Example: Move mouse
   client.write(JSON.stringify({ type: 'click', x: 796, y: 618 }));
 });
 
