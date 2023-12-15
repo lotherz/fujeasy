6d8857660437c1648277a7cda5570f3d2b417e1e
Kyle Lotherington <kyle.lotherington@gmail.com>
2023-12-15 15:44:58 +1100

    Improve TCP handling between server & client

--- src/app.js
+++ src/app.js
@@ -1,33 +1,26 @@
 const net = require('net');
 const express = require('express');
 
-//Set up express server
 const app = express();
 const port = 3000;
 
-//Set up express to serve static files
 app.use(express.static('../public'));
 
-//Start server
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
 
 const client = new net.Socket();
+client.on('error', (err) => {
+    console.error('Socket error:', err);
+});
 
 function click(x, y) {
     client.write(JSON.stringify({ type: 'click', x: x, y: y }));
 }
 
 function settings(film_type, look, border, file_format) {
-    init_settings = [
-        film_type = film_type,
-        look = look,
-        border = border,
-        file_format = file_format
-    ]
-
-    return init_settings;
+    return { film_type, look, border, file_format };
 }
 
 client.connect(8080, '192.168.1.20', () => {
@@ -37,11 +30,11 @@ client.connect(8080, '192.168.1.20', () => {
         click(301, 173);
         click(404, 289);
     }
+    client.end(); // Close the connection when done
 });
 
 client.on('data', (data) => {
     console.log('Received: ' + data);
-    client.destroy(); // Destroy after operation
 });
 
 client.on('close', () => {
--- src/server.py
+++ src/server.py
@@ -7,7 +7,7 @@ pyautogui.PAUSE = 0.5
 def process_command(command):
     if command['type'] == 'click':
         pyautogui.click(command['x'], command['y'])
-        print("Clicking at: " + str(command['x']) + ", " + str(command['y']))
+        print(f"Clicking at: {command['x']}, {command['y']}")
 
 def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,9 +17,15 @@ def start_server():
     while True:
         client_socket, addr = server_socket.accept()
         print("Connection has been established.")
-        
-        data = client_socket.recv(1024)
-        command = json.loads(data.decode('utf-8'))
+
+        full_data = b''
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            full_data += data
+
+        command = json.loads(full_data.decode('utf-8'))
         process_command(command)
 
         client_socket.close()
