f5e107768b1c56760ab6271b2072342f96a452e7
Kyle Lotherington <kyle.lotherington@gmail.com>
2024-03-21 19:40:38 +1100

    Enlarging and testing 13

--- server/ocr.py
+++ server/ocr.py
@@ -8,6 +8,11 @@ image_path = r'C:\s_test.png'
 # Open and convert to grayscale
 image = Image.open(image_path).convert('L')
 
+# Rescale the image, increasing its size by a factor (e.g., 2x, 3x, etc.)
+factor = 3
+new_size = (int(image.width * factor), int(image.height * factor))
+image = image.resize(new_size, Image.ANTIALIAS)
+
 # Apply a median filter for noise reduction
 image = image.filter(ImageFilter.MedianFilter(size=1))
 
@@ -15,7 +20,7 @@ image = image.filter(ImageFilter.MedianFilter(size=1))
 image.save(r'C:\preprocessed_image.png')
 
 # Now pass the preprocessed image to pytesseract
-text = pytesseract.image_to_string(image, config='-c tessedit_char_whitelist=0123456789 -psm 7')
+text = pytesseract.image_to_string(image, config='-psm 13')
 
 if (text) :
     print("Found text: " + text)
