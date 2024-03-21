1a76b117cbbedb61d6ee38109c72f113bb5f6168
Kyle Lotherington <kyle.lotherington@gmail.com>
2024-03-21 19:23:15 +1100

    Added error logging ocr.py

--- server/ocr.py
+++ server/ocr.py
@@ -4,9 +4,15 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 image_path = r'C:\s_test.png'
+
 image = Image.open(image_path).convert('L')  # Convert to grayscale
 image = ImageOps.autocontrast(image)  # Improve contrast
 image = image.filter(ImageFilter.MedianFilter())  # Apply a median filter for noise reduction
-text = pytesseract.image_to_string(image, config='--psm 7')
 
-print(text)
+text = pytesseract.image_to_string(image, config='-psm 7')
+
+if (text) {
+    print("Found text: " + text)
+} else {
+    print("ERROR: Text not found")
+}
