3c929983a1e7ef089c933d128a983790ab772356
Kyle Lotherington <kyle.lotherington@gmail.com>
2024-05-01 15:16:33 +1000

    testing without morph

--- server/server.py
+++ server/server.py
@@ -133,14 +133,7 @@ def read_job_no(image) :
     image = image.point(lambda p: p > threshold_value and 255)
     
     print("Thresholding applied...")
-    
-    # Convert to NumPy array for OpenCV operations
-    image_np = np.array(image)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (4,4))
-    image_np = cv2.morphologyEx(image_np, cv2.MORPH_CLOSE, kernel, iterations=2)
-    image_np = 255 - image_np  # Invert image for better OCR
-    image = Image.fromarray(image_np)
-    print("Morphological operations applied...")
+   
     
     print("Image inverted...")
     
