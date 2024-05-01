3583d1208398d298d628da13f3adeab320acd895
Kyle Lotherington <kyle.lotherington@gmail.com>
2024-05-01 13:10:12 +1000

    Unseparated out OCR ffs

--- server/server.py
+++ server/server.py
@@ -7,8 +7,7 @@ import time
 import numpy as np
 from PIL import Image, ImageFilter, ImageOps, ImageChops
 import asyncio
-from ocr import read_job_no
-
+import pytesseract
 
 fp = "//Mac/Home/Documents/fujeasy/server/screenshots"
 
@@ -90,7 +89,64 @@ def get_look():
     print("Look not found, defaulting to standard")
     return "standard"
 
-########### FIX ############
+
+def read_job_no(input_image) :
+
+    print("Reading job number...")
+    
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+    print("Tesseract path set...")
+    
+    print("Image object type:", type(image))
+    print("Image.size type:", type(image.size))
+
+    # Use the size attribute to get width and height
+    try:
+    # Assuming `image` is the PIL Image object
+        width, height = image.size
+        print("Image size: ", width, height)
+    except Exception as e:
+        print("Error accessing image size:", e)
+    
+    # Rescale the image, increasing its size by a factor (e.g., 2x, 3x, etc.)
+    factor = 10
+    new_size = (int(width * factor), int(height * factor))
+    image = image.resize(new_size, Image.ANTIALIAS)
+    
+    print("Image resized...")
+
+    # Apply Gaussian blur to create a low-pass filtered image
+    # The radius defines the strength of the blur
+    low_pass = image.filter(ImageFilter.GaussianBlur(radius=100))
+    
+    print("Low-pass filter applied...")
+
+    # Subtract the low-pass filtered image from the original image
+    # to achieve a high-pass filtered effect
+    image = ImageChops.subtract(image, low_pass)
+    
+    print("High-pass filter applied...")
+
+    #thresholding
+    threshold_value = 2
+    image = image.point(lambda p: p > threshold_value and 255)
+    
+    print("Thresholding applied...")
+
+    #invert image
+    image = ImageOps.invert(image)
+    
+    print("Image inverted...")
+    
+    #image.save(r'C:\preprocessed_image.png')
+
+    # Now pass the preprocessed image to pytesseract
+    job_no = pytesseract.image_to_string(image, config='-psm 7 nobatch digits')
+    
+    print("Job number read...")
+    
+    return(job_no)
 
 def get_job_number(byte_data):
     
